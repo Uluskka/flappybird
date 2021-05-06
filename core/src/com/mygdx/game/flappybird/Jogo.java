07a3240c35b82fd952e12f21b3c5dec98cc4cc9f
Uluskka <luska.pontes.freitas@hotmail.com>
2021-05-06 00:48:28 -0300

    Update Jogo.java

--- core/src/com/mygdx/game/flappybird/Jogo.java
+++ core/src/com/mygdx/game/flappybird/Jogo.java
@@ -13,11 +13,15 @@ public class Jogo extends ApplicationAdapter {
 
 	private  float larguraDispositivo;
 	private  float alturaDispositivo;
+
+	private int movimentaY = 0;
+	private int movimentaX = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		fundo = new Texture("fundo.png"); //informa o item fundo.png.
+		passaro = new Texture("passaro1.png");//informa o passaro.png
 
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDispositivo = Gdx.graphics.getHeight();
@@ -29,8 +33,12 @@ public class Jogo extends ApplicationAdapter {
 		//informa para renderizar o item dentro da tela do celular.
 		batch.begin();
 
-		batch.draw(fundo, 100, 100, larguraDispositivo, alturaDispositivo); //desenha o objeto,
+		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo); //desenha o objeto.
+		batch.draw(passaro, movimentaX, movimentaY);
 
+
+		movimentaX++; //eixo X.
+		//movimentaY++; //eixo Y.
 		batch.end(); //encerra o processo.
 	}
 	
