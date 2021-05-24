a9e5f63762e55655f2bbb8730a9b1e6ef504965e
Uluskka <luska.pontes.freitas@hotmail.com>
2021-05-24 15:45:53 -0300

    Update 24/05

--- core/src/com/mygdx/game/flappybird/Jogo.java
+++ core/src/com/mygdx/game/flappybird/Jogo.java
@@ -33,11 +33,13 @@ public class Jogo extends ApplicationAdapter {
 	//private  float posicaoCano;
 	private int pontos = 0;
 	private int gravidade = 0;
+	private int estadoJogo = 0;
 
 	private Texture[]passaros;
 	private Texture fundo;
 	private Texture canoBaixo;
 	private Texture canoTopo;
+	private Texture gameOver;
 	private SpriteBatch batch;
 
 	BitmapFont textoPontucao;
@@ -48,6 +50,9 @@ public class Jogo extends ApplicationAdapter {
 	private Circle circuloPassaro;
 	private Rectangle retanguloCanoCima;
 	private Rectangle retanguloCanoBaixo;
+
+	BitmapFont textoReiniciar;
+	BitmapFont textoMelhorPontuacao;
 
 	@Override
 	public void create () {
@@ -71,6 +76,14 @@ public class Jogo extends ApplicationAdapter {
 		textoPontucao.setColor(Color.WHITE);
 		textoPontucao.getData().setScale(10);
 
+		textoReiniciar = new BitmapFont(); //renderiza minha tela de gameover.
+		textoReiniciar.setColor(Color.GREEN);
+		textoReiniciar.getData().setScale(3);
+
+		textoMelhorPontuacao = new BitmapFont(); //renderiza a melhor pontuacao.
+		textoMelhorPontuacao.setColor(Color.RED);
+		textoMelhorPontuacao.getData().setScale(3);
+
 		//collider
 		shapeRenderer = new ShapeRenderer();
 		circuloPassaro = new Circle();
@@ -81,7 +94,7 @@ public class Jogo extends ApplicationAdapter {
 
 	private void inicializaTexturas(){
 
-		//Materializa as imagens na tela do aplicativo.
+		//Materializa as imagens na tela do aplicativo para a animaçao.
 		passaros = new Texture[3];
 		passaros[0] = new Texture("passaro1.png");
 		passaros[1] = new Texture("passaro2.png");
@@ -90,6 +103,7 @@ public class Jogo extends ApplicationAdapter {
 		fundo = new Texture("fundo.png");
 		canoBaixo = new Texture("cano_baixo_maior.png");
 		canoTopo = new Texture("cano_topo_maior.png");
+		gameOver = new Texture("game_over.png");
 
 	}
 
@@ -123,6 +137,7 @@ public class Jogo extends ApplicationAdapter {
 
 		if (bateuCanoBaixo || bateuCanoCima){
 			Gdx.app.log("Log","Bateu");
+			estadoJogo = 2;
 
 		}
 	}
@@ -135,37 +150,51 @@ public class Jogo extends ApplicationAdapter {
 				passouCano = true;
 			}
 		}
+		//animacao das asas do passaro.
+		variacao += Gdx.graphics.getDeltaTime() * 10;
+		if(variacao > 3)
+			variacao = 0;
 
 	}
 
 	private void verificaEstadoJogo() {
-		//movimenta os cano.
-		posicaoCanoHorizontal -= Gdx.graphics.getDeltaTime() * 200;
-		if(posicaoCanoHorizontal < - canoBaixo.getHeight()){
-			posicaoCanoHorizontal = larguraDispositivo;
-			posicaoCanoVertical = random.nextInt(400) -200;
-			passouCano = false;
+		//metodo de estado 0 inicia o jogo parado botao de gatilho para o estado 1 é toqueTela ao tocar muda de estado 0 para estado 1 iniciando o game.
+		boolean toqueTela = Gdx.input.justTouched();
+		if(estadoJogo == 0){
 
-			boolean toqueTela = Gdx.input.justTouched();
-			if (Gdx.input.justTouched()){
-				gravidade = -25;
+			if (toqueTela){
+				gravidade = -15;
+				estadoJogo = 1;
 			}
+
+		} else if (estadoJogo ==1){
+
+			if (toqueTela){
+				gravidade = -15;
+
+			}
+
+			//movimenta os cano.
+			posicaoCanoHorizontal -= Gdx.graphics.getDeltaTime() * 200;
+			if(posicaoCanoHorizontal < - canoBaixo.getHeight()){
+				posicaoCanoHorizontal = larguraDispositivo;
+				posicaoCanoVertical = random.nextInt(400) -200;
+				passouCano = false;
+		}
 			if (posicaoInicialVerticalPassaro > 0 || toqueTela)
 				posicaoInicialVerticalPassaro = posicaoInicialVerticalPassaro - gravidade;
 
-			variacao += Gdx.graphics.getDeltaTime() * 10;
-			if(variacao > 3)
-				variacao = 0;
+				gravidade++;
+			} else if(estadoJogo == 2){
 
-			gravidade++;
-
+			}
 
 		}
-	}
+
 
 	private void desenharTexturas() {
 
-		//desenha e renderiza as imagens do projeto.
+		//desenha e renderiza as texturas do projeto.
 		batch.begin();
 
 		batch.draw(fundo,0,0,larguraDispositivo,alturaDispositivo);
@@ -173,6 +202,15 @@ public class Jogo extends ApplicationAdapter {
 		batch.draw(canoBaixo, posicaoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + posicaoCanoVertical);
 		batch.draw(canoTopo, posicaoCanoHorizontal, alturaDispositivo / 2 + espacoEntreCanos / 2 + posicaoCanoVertical);
 		textoPontucao.draw(batch, String.valueOf(pontos), larguraDispositivo / 2, alturaDispositivo -100);
+
+		if (estadoJogo == 2){
+			batch.draw(gameOver, larguraDispositivo / 2 - gameOver.getWidth() / 2, alturaDispositivo / 2);
+			textoReiniciar.draw(batch, "TOQUE NA TELA PARA REINICIAR!", larguraDispositivo / 2 -200,
+					alturaDispositivo / 2 - gameOver.getHeight() / 2);
+			textoMelhorPontuacao.draw(batch, "SUA MELHOR PONTUAÇAO É : 0 PONTOS!", larguraDispositivo / 2 - 300,
+					alturaDispositivo / 2 - gameOver.getHeight() * 2);
+		}
+
 
 		batch.end(); //encerra o processo.
 	}
